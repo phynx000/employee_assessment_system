c4e4cecbbfe30a70369cbba2ffb218e26ba9d484
phynx000 <danhphuoctoan.w@gmail.com>
2025-03-21 09:26:56 +0700

    initial

---
+++ src/main/java/com/example/employee_assessment_system/EmployeeAssessmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.employee_assessment_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAssessmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAssessmentSystemApplication.class, args);
+	}
+
+}
